d2a18ecbcd4721b7bbf3a4a0e76a58280f20faf9
LAnastasiia <lazorenko@ucu.edu.ua>
2019-01-17 11:38:00 +0000

    latest_version

--- assistant_webserver/flask_web_server.py
+++ assistant_webserver/flask_web_server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from auth import auth_error, get_coords
 import rospy
 import rospkg
@@ -8,37 +8,24 @@ import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='templates')
 app.jinja_env.auto_reload = True
 # app.config['TEMPLATES_AUTO_RELOAD'] = False
 app.secret_key = "super&key*secret"
 
-def move_base(x, y, client):
-    goal = MoveBaseGoal()
-    goal.target_pose.header.frame_id = "map"
-    goal.target_pose.header.stamp = rospy.Time.now()
-    goal.target_pose.pose.position.x = x
-    goal.target_pose.pose.position.y = y
-    goal.target_pose.pose.orientation.w = 1.0
-    client.send_goal(goal)
-    wait = client.wait_for_result()
-    if not wait:
-        rospy.logerr("Action server not available!")
-        rospy.signal_shutdown("Action server not available!")
-    else:
-        return client.get_result()
 
 @app.route('/', methods=["GET", "POST"])
 def start():
     if request.method == "POST":
         if not auth_error(request.form.get("user_name"), request.form.get("password")):
-            # succ_msg = request.form.get("user_name")
-            return render_template("search.html")
+            return  send_from_directory(app.static_folder, "search.html")
+            # return render_template("search.html")
         else:
-            # fail_msg = "Wrong passwod or userame, try again."
-            return render_template('main.html')
-    # init_msg = "Welcome!"
-    return render_template('main.html')
+            return send_from_directory(app.static_folder, "main.html")
+            # return render_template('main.html')
+
+    return send_from_directory(app.static_folder, "main.html")
+    # return render_template('main.html')
 
 
 @app.route('/search', methods=["GET", "POST"])
@@ -47,8 +34,20 @@ def search():
         worker = request.form.get("person")
         if worker:
             execfile('main_script.py')
-            return render_template("assistant.html")
+            return send_from_directory(app.static_folder, "assistant.html")
+            # return render_template("assistant.html")
 
-    return render_template("search.html")
+    return send_from_directory(app.static_folder, "search.html")
+    # return render_template("search.html")
+
+
 
 app.run(debug=True, use_reloader=False)
+
+#ToDo:
+# add custom messages into templates
+    # succ_msg = request.form.get("user_name")
+    # fail_msg = "Wrong passwod or userame, try again."
+    # init_msg = "Welcome!"
+
+
